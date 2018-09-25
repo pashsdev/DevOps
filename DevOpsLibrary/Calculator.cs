1e2f903452c9eb3091e18292dda2803b61e7fe70
Prakash Selvaraj <prakash.selvaraj@testsys.com>
2018-09-25 20:31:20 +0530

    Initial commit

---
+++ DevOpsLibrary/Calculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DevOpsLibrary
+{
+    public class Calculator
+    {
+        public Int64 Add(Int64 a, Int64 b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ DevOpsTest/Whitelist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit;
+using NUnit.Framework;
+using DevOpsLibrary;
+
+namespace DevOpsTest
+{
+    [TestFixture]
+    public class Whitelist
+    {
+        [TestCase(1,2,3)]
+        //[TestCase(3,2,4)]
+        public void AddTest(Int64 a, Int64 b,Int64 expectedResult)
+        {
+            Calculator calc = new Calculator();
+            Int64 result = calc.Add(a, b);
+            Assert.AreEqual(expectedResult, result);
+        }
+    }
+}
